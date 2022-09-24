0d53068ec80f413bd94eb8619d2465da99c95556
tanpro1998 <davidtanpro1998@gmail.com>
2022-09-24 15:07:40 +0700

    commit

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://flash-car-shop.herokuapp.com";
+const BASE_URL = "https://flash-car-booking.herokuapp.com/";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
